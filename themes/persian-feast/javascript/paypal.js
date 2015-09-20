be92009c08be6b98794bcf338da49909e2a685cd
Luke Hardiman <luke@hardiman.co.nz>
2015-09-20 19:53:06 +1200

    Started new paypal shopping cart system -started base for new paypal shopping cart, will move to module after finished

--- themes/persian-feast/javascript/paypal.js
+++ themes/persian-feast/javascript/paypal.js
@@ -4,19 +4,44 @@
  */
 console.log("paypal.js : loading");
 
+"use strict";
+
+var ordering = [], $shoppingCartContainer = null;
+
 $(document).ready(function () {
 
     //declare our shopping cart element
-    var shoppingCartContainer = $("#shoppingCartContainer");
+    $shoppingCartContainer = $(".shoppingCartContainer");
 
     //adding item to cart
-    $(".addToCartDiv").click(function () {
-
-        var data = $('.item', this).data('item');
-        console.log("click adding", data);
-    });
-
+    $(".addToCartDiv").click(addToCart);
+    $("#checkOut").click(function () {
+        alert("Sorry not complete yet.");
+    })
 
 
 });
 
+
+function addToCart() {
+    //lets get the data
+    var data = $('.item', this).data('item');
+
+    //check the data
+    if (typeof data.title != "string" || typeof data.price != "string") {
+        console.log("data", data);
+        throw new Error("Error missing data");
+    }
+
+
+    //add data to order
+    ordering[data.item_id] = data;
+
+    //append data to order this will be moved to another function
+    $('#item', $shoppingCartContainer).append('<p>' + data.title + '</p>');
+    $('#quantity', $shoppingCartContainer).append('<p>1</p><br/>');  //@todo count from ordering array
+    $('#price', $shoppingCartContainer).append('<p>' + data.price + '</p>');  //@todo complete total
+    $('#options', $shoppingCartContainer).append('<p><i class="fa fa-remove"></i></p>');  //@todo complete total
+    console.log("click adding", data);
+    console.log("ordering", ordering);
+}
